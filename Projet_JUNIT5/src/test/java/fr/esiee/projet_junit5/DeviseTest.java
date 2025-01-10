6af276f19fe0fd02f4c5fbbaf270c0ef43a4c313
eljhdbo <nezaydabo@gmail.com>
2025-01-10 16:19:24 +0100

    projet junit5

---
+++ Projet_JUNIT5/src/test/java/fr/esiee/projet_junit5/DeviseTest.java
@@ -0,0 +1,30 @@
+package fr.esiee.projet_junit5;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeviseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getQuantite() {
+    }
+
+    @Test
+    void getMonnaie() {
+    }
+
+    @Test
+    void add() {
+    }
+}
