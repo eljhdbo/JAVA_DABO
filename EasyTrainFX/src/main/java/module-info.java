abb47b12ae4e7ce0948843c63262e532b1ebb686
eljhdbo <nezaydabo@gmail.com>
2025-01-24 14:13:54 +0100

    Mise à jour EasyTrain 4

--- EasyTrainFX/src/main/java/module-info.java
+++ EasyTrainFX/src/main/java/module-info.java
@@ -1,11 +1,8 @@
-module eu.hautil.easytrainfx {
+module fr.esiee.easytrainfx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
 
-
     opens fr.esiee.easytrainfx to javafx.fxml;
     exports fr.esiee.easytrainfx;
-    exports fr.esiee;
-    opens fr.esiee to javafx.fxml;
 }
